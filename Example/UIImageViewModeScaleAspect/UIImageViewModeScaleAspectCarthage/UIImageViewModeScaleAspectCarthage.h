d957c75e01349898e7ded5be93154dc2860f872d
Administrator <vivien.cormier@gmail.com>
2016-12-08 22:49:57 +0100

    [F] Enable carthage auto build

---
+++ Example/UIImageViewModeScaleAspect/UIImageViewModeScaleAspectCarthage/UIImageViewModeScaleAspectCarthage.h
@@ -0,0 +1,19 @@
+//
+//  UIImageViewModeScaleAspectCarthage.h
+//  UIImageViewModeScaleAspectCarthage
+//
+//  Created by Vivien Cormier on 08/12/2016.
+//  Copyright © 2016 Vivien Cormier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIImageViewModeScaleAspectCarthage.
+FOUNDATION_EXPORT double UIImageViewModeScaleAspectCarthageVersionNumber;
+
+//! Project version string for UIImageViewModeScaleAspectCarthage.
+FOUNDATION_EXPORT const unsigned char UIImageViewModeScaleAspectCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIImageViewModeScaleAspectCarthage/PublicHeader.h>
+
+
